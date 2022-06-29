4ebc5d993ba3eabcb8ac9d6c71969a2ba1b1494a
RdrBux <rodriguezrodrigoemmanuel@gmail.com>
2022-06-29 15:02:55 -0300

    Add selector for one answer in each question

--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Quiz from './Quiz';
+import { nanoid } from 'nanoid';
 
 export default function MainScreen() {
   const [trivia, setTrivia] = useState([]);
@@ -10,6 +11,15 @@ export default function MainScreen() {
       txt.innerHTML = html;
       return txt.value;
     }
+
+    function formatAnswer(answerElement, correct = false) {
+      return {
+        answer: decodeHtml(answerElement),
+        isCorrect: correct,
+        isSelected: false,
+        id: nanoid(),
+      };
+    }
     async function getData() {
       const resp = await fetch(
         'https://opentdb.com/api.php?amount=5&type=multiple'
@@ -19,41 +29,44 @@ export default function MainScreen() {
         data.results.map((element) => ({
           question: decodeHtml(element.question),
           answers: [
-            {
-              answer: decodeHtml(element.correct_answer),
-              isCorrect: true,
-              isSelected: false,
-            },
-            {
-              answer: decodeHtml(element.incorrect_answers[0]),
-              isCorrect: false,
-              isSelected: false,
-            },
-            {
-              answer: decodeHtml(element.incorrect_answers[1]),
-              isCorrect: false,
-              isSelected: false,
-            },
-            {
-              answer: decodeHtml(element.incorrect_answers[2]),
-              isCorrect: false,
-              isSelected: false,
-            },
+            formatAnswer(element.correct_answer, true),
+            formatAnswer(element.incorrect_answers[0]),
+            formatAnswer(element.incorrect_answers[1]),
+            formatAnswer(element.incorrect_answers[2]),
           ],
+          id: nanoid(),
         }))
       );
     }
     getData();
   }, []);
 
-  const questions = trivia.map((quiz, index) => (
+  const questions = trivia.map((quiz) => (
     <Quiz
-      key={index}
+      key={quiz.id}
+      id={quiz.id}
       question={quiz.question}
       answers={quiz.answers}
-      id={index}
+      selectAnswer={selectAnswer}
     />
   ));
+
+  function selectAnswer(answId, quizId) {
+    setTrivia((prevState) =>
+      prevState.map((quiz) =>
+        quiz.id === quizId
+          ? {
+              ...quiz,
+              answers: quiz.answers.map((answer) =>
+                answer.id === answId
+                  ? { ...answer, isSelected: true }
+                  : { ...answer, isSelected: false }
+              ),
+            }
+          : { ...quiz }
+      )
+    );
+  }
 
   return (
     <main className="main">
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
 export default function Quiz(props) {
-  const { question, answers } = props;
-  const allAnswers = answers.map((answ, index) => (
-    <div className="quiz--answer" key={index}>
+  const { id, question, answers, selectAnswer } = props;
+
+  const allAnswers = answers.map((answ) => (
+    <div
+      className="quiz--answer"
+      onClick={() => selectAnswer(answ.id, id)}
+      style={{ backgroundColor: answ.isSelected ? '#D6DBF5' : '#F5F7FB' }}
+      key={answ.id}
+      id={answ.id}
+    >
       {answ.answer}
     </div>
   ));
+
   return (
     <div className="quiz">
       <h3 className="quiz--question">{question}</h3>
