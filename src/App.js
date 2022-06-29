48cf670e9b0cd8140addc1d257a2e842447cc707
RdrBux <rodriguezrodrigoemmanuel@gmail.com>
2022-06-29 19:25:40 -0300

    Display StartScreen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import MainScreen from './components/MainScreen';
 import StartScreen from './components/StartScreen';
+import React, { useState } from 'react';
 
 function App() {
+  const [startGame, setStartGame] = useState(false);
+
+  function handleStart() {
+    setStartGame(true);
+  }
+
   return (
     <div className="App">
-      <StartScreen />
+      {startGame ? <MainScreen /> : <StartScreen handleStart={handleStart} />}
     </div>
   );
 }
--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
-export default function StartScreen() {
+export default function StartScreen(props) {
   return (
     <div className="start">
       <h1 className="start--title">Quizzical</h1>
       <h3 className="start--description">
         Prove your knowledge and enjoy a fantastic reward!
       </h3>
-      <button className="start--button button">Start quiz</button>
+      <button onClick={props.handleStart} className="start--button button">
+        Start quiz
+      </button>
     </div>
   );
 }
